ec89cf77d672aa79b5eea547d84fa22bce5e1b6a
Andrey Syutkin <syutkin@gmail.com>
2021-07-19 22:05:02 +0300

    Start next game without restart

---
+++ bin/models/game.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:ansicolor/ansicolor.dart';
+
+import 'cell.dart';
+import 'player.dart';
+
+class Game {
+  Player player1;
+  Player player2;
+
+  Game(this.player1, this.player2);
+
+  void playGame() {
+    player1.placeShips();
+
+    for (var i = 0; i < 100; i++) {
+      stdout.writeln('');
+    }
+
+    player2.placeShips();
+
+    for (var i = 0; i < 100; i++) {
+      stdout.writeln('');
+    }
+
+    var _currentPlayer = player1;
+
+    Player currentPlayer() {
+      if (_currentPlayer == player1) {
+        return player1;
+      } else {
+        return player2;
+      }
+    }
+
+    Player anotherPlayer() {
+      if (_currentPlayer == player1) {
+        return player2;
+      } else {
+        return player1;
+      }
+    }
+
+    var rng = Random();
+
+    if (rng.nextInt(2) == 1) {
+      _currentPlayer = anotherPlayer();
+    }
+
+    while (player1.isAlive && player2.isAlive) {
+      currentPlayer().battleField.printField();
+      stdout.write('Игрок ${currentPlayer().name} делает выстрел: ');
+
+      // var x = currentPlayer().playerField.readCoordinate('x');
+      // var y = currentPlayer().playerField.readCoordinate('y');
+      var shot = currentPlayer().playerField.readCoordinate();
+
+      var shotResult = anotherPlayer().playerField.doShot(shot.x, shot.y);
+      currentPlayer().battleField.doShot(shot.x, shot.y, shotResult);
+      if (shotResult is EmptyCell) {
+        _currentPlayer = anotherPlayer();
+      }
+    }
+    var pen = AnsiPen()..cyan();
+    stdout.writeln(pen('Игрок ${_currentPlayer.name} победил!'));
+  }
+}
--- bin/sea_battle.dart
+++ bin/sea_battle.dart
@@ -1,70 +1,38 @@
 import 'dart:io';
-import 'dart:math';
 
-import 'package:ansicolor/ansicolor.dart';
-
-import 'models/cell.dart';
+import 'models/game.dart';
 import 'models/player.dart';
 
 void main(List<String> arguments) {
   const FILED_LENGTH = 10;
 
-  stdout.write('Введите имя первого игрока: ');
-  var player1 = Player(stdin.readLineSync() ?? '', FILED_LENGTH);
+  var playGame = true;
 
-  stdout.write('Введите имя второго игрока: ');
-  var player2 = Player(stdin.readLineSync() ?? '', FILED_LENGTH);
+  while (playGame) {
+    stdout.write('Введите имя первого игрока: ');
+    var player1 = Player(stdin.readLineSync() ?? '', FILED_LENGTH);
 
-  player1.placeShips();
+    stdout.write('Введите имя второго игрока: ');
+    var player2 = Player(stdin.readLineSync() ?? '', FILED_LENGTH);
 
-  for (var i = 0; i < 100; i++) {
-    stdout.writeln('');
+    var game = Game(player1, player2);
+
+    game.playGame();
+
+    int? continueGame;
+
+    do {
+      stdout.write('Начать новую игру: 1 - да; 2 - нет: ');
+      continueGame =
+          int.tryParse((stdin.readLineSync() ?? '').trim().replaceAll(' ', ''));
+
+      if (continueGame == 2) {
+        playGame = false;
+      }
+
+      if (continueGame != 2 && continueGame != 1) {
+        stdout.writeln('Непонятный ответ');
+      }
+    } while (continueGame != 2 && continueGame != 1);
   }
-
-  player2.placeShips();
-
-  for (var i = 0; i < 100; i++) {
-    stdout.writeln('');
-  }
-
-  var _currentPlayer = player1;
-
-  Player currentPlayer() {
-    if (_currentPlayer == player1) {
-      return player1;
-    } else {
-      return player2;
-    }
-  }
-
-  Player anotherPlayer() {
-    if (_currentPlayer == player1) {
-      return player2;
-    } else {
-      return player1;
-    }
-  }
-
-  var rng = Random();
-
-  if (rng.nextInt(2) == 1) {
-    _currentPlayer = anotherPlayer();
-  }
-
-  while (player1.isAlive && player2.isAlive) {
-    currentPlayer().battleField.printField();
-    stdout.write('Игрок ${currentPlayer().name} делает выстрел: ');
-
-    // var x = currentPlayer().playerField.readCoordinate('x');
-    // var y = currentPlayer().playerField.readCoordinate('y');
-    var shot = currentPlayer().playerField.readCoordinate();
-
-    var shotResult = anotherPlayer().playerField.doShot(shot.x, shot.y);
-    currentPlayer().battleField.doShot(shot.x, shot.y, shotResult);
-    if (shotResult is EmptyCell) {
-      _currentPlayer = anotherPlayer();
-    }
-  }
-  var pen = AnsiPen()..cyan();
-  stdout.writeln(pen('Игрок ${_currentPlayer.name} победил!'));
 }
